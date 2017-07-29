889b909a0d4c1e122b70152a26acea4f5c135bdb
ADDERMEDINA\AdderMedina <adderly.polanco@minerd.gob.do>
2017-07-29 10:33:00 -0400

    Ultimos cambios

--- app/Http/Requests/AlmacenFormRequest.php
+++ app/Http/Requests/AlmacenFormRequest.php
@@ -24,7 +24,7 @@ class AlmacenFormRequest extends FormRequest
     public function rules()
     {
         return [
-            'descripcion' => 'required'
+            'descripcion' => 'required|unique:almacenes'
         ];
     }
 }
